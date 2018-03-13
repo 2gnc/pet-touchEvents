62667fe512d9dd8e5e3c796c2a5ca6ea66642eb0
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-03-13 17:56:32 +0300

    прибила старую версию

--- public/app.js
+++ public/app.js
@@ -1,22 +1,3 @@
-window.addEventListener( 'DOMContentLoaded', function( e ){
-
-	const b1 = document.getElementById( 'one' );
-	const b2 = document.getElementById( 'two' );
-	const b3 = document.getElementById( 'three' );
-	const b4 = document.getElementById( 'four' );
-	const b5 = document.getElementById( 'five' );
-	const b6 = document.getElementById( 'six' );
-	const b7 = document.getElementById( 'seven' );
-	const b8 = document.getElementById( 'eigth' );
-	const b9 = document.getElementById( 'nine' );
-	const msg = document.querySelector( 'p' );
-
-	let mc = new Hammer.Manager(b2);
-	mc.add( new Hammer.Tap({ event: 'singletap' }) );
-
-
-	mc.on("singletap", function(e) {
-		msg.innerText = e.type;
-	});
+window.addEventListener( 'DOMContentLoaded', ( e ) => {
 
 });
