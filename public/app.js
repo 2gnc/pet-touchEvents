92bf74fcefb00646577058b19183a20a6416689d
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-13 23:25:45 +0300

    добавила обработку двойного тапа

--- public/app.js
+++ public/app.js
@@ -1,13 +1,32 @@
 window.addEventListener( 'DOMContentLoaded', ( e ) => {
 	const invader = document.querySelector( '.invader' );
+	const invaderEls = document.querySelectorAll( '.invader_el' );
 	const manager = new Hammer.Manager( invader );
 	const DoubleTap = new Hammer.Tap({
 		event: 'doubletap',
 		taps: 2
 	});
+	const guns = document.querySelectorAll( '.fa-star' );
+
+	let isGunman = false;
+
 	manager.add(DoubleTap);
 
-	manager.on('doubletap', (e) => {
-		});
+	console.log( invaderEls  );
 
-});
+	manager.on('doubletap', (e) => {
+        if (!isGunman) {
+            isGunman = true;
+            guns.forEach( (item)=> {
+                item.classList.toggle( 'fa-star_animated' )
+            });
+            setTimeout(()=>{
+                guns.forEach( (item)=> {
+                    item.classList.toggle( 'fa-star_animated' )
+                });
+                isGunman = false;
+            }, 2200)
+        }
+    })
+
+})
