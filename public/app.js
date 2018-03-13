2d9a5ec9dcd95404317c9dda32ba2995371d97e6
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-03-13 18:43:35 +0300

    заготовка для даблтапа

--- public/app.js
+++ public/app.js
@@ -1,3 +1,13 @@
 window.addEventListener( 'DOMContentLoaded', ( e ) => {
+	const invader = document.querySelector( '.invader' );
+	const manager = new Hammer.Manager( invader );
+	const DoubleTap = new Hammer.Tap({
+		event: 'doubletap',
+		taps: 2
+	});
+	manager.add(DoubleTap);
+
+	manager.on('doubletap', (e) => {
+		});
 
 });
