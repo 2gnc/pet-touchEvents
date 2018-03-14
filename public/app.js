0ac1bc1a0224b7a04f39a981cd8fde4fa256dcbf
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-03-14 12:50:34 +0300

    test2

--- public/app.js
+++ public/app.js
@@ -1,21 +1,20 @@
 window.addEventListener( 'DOMContentLoaded', ( e ) => {
 	const invader = document.querySelector( '.invader' );
-	const invader2 = document.querySelector( '.invader' );
 	const invaderEls = document.querySelectorAll( '.invader_el' );
 	const manager = new Hammer.Manager( invader );
 	const DoubleTap = new Hammer.Tap({
 		event: 'doubletap',
 		taps: 2
 	});
-
-	let invHammer = new Hammer( invader2 );
-
 	const guns = document.querySelectorAll( '.fa-star' );
+
+    let hammer = new Hammer( invader );
 
 	let isGunman = false;
 
-	manager.add( DoubleTap );
-	
+	manager.add(DoubleTap);
+
+	console.log( invaderEls  );
 
 	manager.on('doubletap', (e) => {
         if (!isGunman) {
@@ -32,9 +31,14 @@ window.addEventListener( 'DOMContentLoaded', ( e ) => {
         }
     })
 
-	invHammer.on( 'swiperight', (e) => {
-		console.log( 'swipe' );
-		invaderEls[0].innerHTML = 'ttt';
-	} )
+    hammer.on( 'panleft', (e) => {
+        invaderEls[0].innerHTML = 'pa';
+        console.log('pa');
+    } )
+
+    hammer.on( 'swipeleft', (e) => {
+        invaderEls[1].innerHTML = 'vs';
+        console.log('sv');
+    } )
 
 })
